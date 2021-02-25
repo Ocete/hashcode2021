8fc2e083e8e7c3e04a6e020721b7280de24f4212
Pablo Baeyens <pbaeyens31+github@gmail.com>
2021-02-25 20:08:04 +0100

    Fix zeus

--- zeus.sh
+++ zeus.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e
 
-time python3 solution.py a.txt
-time python3 solution.py b.txt
-time python3 solution.py c.txt
-time python3 solution.py d.txt
-time python3 solution.py e.txt
-time python3 solution.py f.txt
+pushd inputs
+time python3 ../solution.py a.txt
+time python3 ../solution.py b.txt
+time python3 ../solution.py c.txt
+time python3 ../solution.py d.txt
+time python3 ../solution.py e.txt
+time python3 ../solution.py f.txt
+popd
